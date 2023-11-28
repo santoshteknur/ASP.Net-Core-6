077c3a95d9d521cd1dc34e0a895bbd7fd0d87d91
santosh teknur <94santosh22@gmail.com>
2023-11-28 08:09:21 +0530

    Learned Action method return type

---
+++ Asp_net_core_empty/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_net_core_empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Display()
+        {
+            return "display method";
+        }
+
+        public int DisplayID(int id) {
+
+            return id;
+
+        }
+    }
+}
--- Asp_net_core_empty/Program.cs
+++ Asp_net_core_empty/Program.cs
@@ -1,27 +1,41 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
 app.UseRouting();
 
-//app.MapGet("/", () => "Worker Process Name : " + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+//MapGet Method use only for Get request
+//app.MapGet("/Get", () => "MApGet Worker Process Name : " + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGet("/Home", async (context) =>
-    {
-        await context.Response.WriteAsync("This is MapGet response..");
-    });
+// Map method is used for all request
+//app.Map("/", () => "Worker Process Name : " + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
 
-    endpoints.MapPost("/Home", async (context) =>
-    {
-        await context.Response.WriteAsync("This is MapPost response..");
-    });
 
-});
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapGet("/Home", async (context) =>
+//    {
+//        await context.Response.WriteAsync("This is MapGet response..");
+//    });
 
-app.Run( async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("Page Not Find");
-});
+//    endpoints.MapPost("/Home", async (context) =>
+//    {
+//        await context.Response.WriteAsync("This is MapPost response..");
+//    });
+
+//});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{Controller=Home}/{action=Index}/{id?}"
+    );
+
+//app.Run( async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Page Not Find");
+//});
 
 app.Run();
 
