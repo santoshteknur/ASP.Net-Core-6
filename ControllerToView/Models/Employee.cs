d740d393ffe3bf8b5a76c40df7e49d41d3864770
santosh teknur <94santosh22@gmail.com>
2023-12-12 19:47:40 +0530

    This project is to understand .net core new fetaures

---
+++ ControllerToView/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace ControllerToView.Models
+{
+    internal class Employee
+    {
+        public int EmpID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ ControllerToView/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControllerToView.Models
+{
+    public class StudentModel
+    {
+        public int rollnumber { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Standard { get; set;}
+    }
+}
